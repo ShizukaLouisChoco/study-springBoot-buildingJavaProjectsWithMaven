b18f8dfbe6e114dbbc881c609e9592f12ea2f1ea
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-02 13:49:29 +0100

    9) Solution LocalTime

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -4,7 +4,7 @@ import java.time.LocalTime;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		LocalTime currentTime = new LocalTime();
+		LocalTime currentTime = LocalTime.now();
 		System.out.println("The current local time is:"+ currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
